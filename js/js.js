ff0d83b762f52572d2a3a2184435c2a146a58fea
nvpost <nvbpost@yandex.ru>
2022-04-01 16:44:26 +0300

    filter

--- js/js.js
+++ js/js.js
@@ -4,7 +4,9 @@ let app = new Vue({
     data() {
         return {
             'tags': [],
-            'news': []
+            'origin_news':[],
+            'news': [],
+            'activeTags':[],
         }
     },
     beforeCreate(){
@@ -15,12 +17,39 @@ let app = new Vue({
             .then((data) => {
                 this.news = data.data
                 this.tags = data.tags
+
+                this.origin_news = this.news
             })
     },
 
     methods:{
         d(date){
             return new Date(date)
+        },
+        addTag(t){
+            if(this.activeTags.indexOf(t)!=-1){
+                console.log(this.activeTags.indexOf(t))
+                this.activeTags.splice(this.activeTags.indexOf(t),1)
+            }else{
+                this.activeTags.push(t)
+                console.log('Добавлен ', t)
+            }
+            this.newSet()
+        },
+        checkTagClass(t){
+            return this.activeTags.indexOf(t)!=-1
+        },
+        newSet(){
+            this.news = this.origin_news
+           ns = this.news.filter(i=>{
+               returnFlag = true;
+               app.activeTags.forEach(j=>{
+                   returnFlag = i.group_id.indexOf(j)!=-1
+               })
+               return returnFlag
+           })
+            this.news = ns
         }
+
     }
 })
